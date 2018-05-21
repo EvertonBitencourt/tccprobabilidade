7a953fb3391b046a43099a3729336da10007368b
EvertonBitencourt <everton.silvaa@gmail.com>
2018-05-20 22:33:01 -0300

    Criação do metodo calcular espaço amostral, desenvolvimento do metodo dialogo completo, incluido todas etapa para resolver um problema de espaço amostral. Criação de um metodo levenshtein váriavel.

--- bot.php
+++ bot.php
@@ -57,35 +57,94 @@ function abrir_banco(){
     );
 }
 
+function ctexto($mensagem, $termo, $margem){ // comparar textos
+    $comp = levenshtein($mensagem,$termo);
+    if($comp<=margem) {
+        return true;
+    }else
+        return false;
+}
+
 function verificarCategoria($texto){
     $db = abrir_banco();
     $categoria = $db->query("SELECT nome FROM categoria") ->fetchAll();
     $cat_lev = "sem categoria";
     foreach ($categoria as $value){
-        $resultado = levenshtein($value,$texto);
-        if($resultado<=2){
+        if(ctexto($texto, $value, 2)){
             $cat_lev = $value;
         }
     }
     return $cat_lev;
 }
 
+function calcular_espaco_amostral($lancamentos, $faces, $detalhado){
+    if($detalhado){
+        $explicacao = "Espaço amostral é o conjunto estabelecido por todos os possíveis resultados de um experimento. Logo devemos elevar a quantidade de lançamentos (";
+        $explicacao = $explicacao.$lancamentos.") à quantidade de faces (".$faces."). Portanto o resultado é: ".pow($lancamentos,$faces);
+        return $explicacao;
+    }else
+    return pow($lancamentos,$faces);
+}
 function dialogo($id, $mensagem){
 
-    $resposta = "em desenvolvimento";
+    $resposta = "não entendi sua mensagem, por favor digite novamente, verificando sua ortografia, eu sou sensivel :( ";
     $db = abrir_banco();
-    $etapa = -1;
+
     $consulta_etapa = $db ->query("SELECT etapa from usuario where id_usuario = $id")->fetch();
     $etapa = $consulta_etapa[0];
-    /*foreach ($consulta_etapa as $value){
-            $etapa = $value;
-        }*/
-    debug($etapa);
-
-    if(strcasecmp ($mensagem,"sim") == 0){
-        $resposta ="Qual é a categoria?"; //teste
+    if($etapa == 1){
+        if(ctexto($mensagem,"sim", 1)){
+            atualizar_etapa($id, 2);
+            $resposta ="Qual é a categoria?"; //teste
+        }
+        if(ctexto($mensagem,"não", 1)){
+            $resposta ="Veja o material que seu professor compartilhou com você em sala de aula ou consulte: https://brasilescola.uol.com.br/matematica/probabilidade.htm \n Após a consulta informe a categoria de seu problema entre as categorias abaixo: \n Espaço Amostral \n Arranjos e Permutações"; //teste
+            atualizar_etapa($id, 2);
+        }
     }
-    if($mensagem == "não"){}
+    if($etapa == 2){
+        if(verificarCategoria($mensagem) == "Espaço Amostral"){
+            $resposta = "Para lhe ajudar melhor preciso saber algumas informações de seu problema, favor responda claramente os próximos questionamentos. Qual objeto está usando?";
+            atualizar_etapa($id, 3);
+        }
+    }
+    if($etapa == 3){
+        if(ctexto($mensagem,"dado", 1)){//atualizar diagram de fluxo de dados com esse item
+            $resposta = "Quantas vezes você irá lançar este objeto";
+            atualizar_etapa($id, 3.1);
+        }
+    }
+    if($etapa == 3.1){
+        if($mensagem >0){
+            $resposta = "Você deseja ter uma resposta detalhada? Responda com sim ou não.";
+            atualizar_etapa($id, 3.2);
+        }
+    }
+    if($etapa == 3.2){
+        $consulta = $db->query("select mensagem from historico where id_usuario=$id order by data_hora")->fetchAll();
+        $valor = $consulta[count($consulta)-2];
+        if(ctexto($mensagem,"não",1)){
+            $resposta= calcular_espaco_amostral($valor,6, false);
+        }else{
+            $resposta=  calcular_espaco_amostral($valor,6, true);
+        }
+        $resposta = $resposta."\n Deseja resolver outro problema?";
+        atualizar_etapa($id, 4);
+    }
+    if($etapa == 4){
+        if(ctexto($mensagem, "sim", 1)){
+                atualizar_etapa($id, 0);
+                $resposta ="Qual é a categoria?";
+            }
+        if(ctexto($mensagem, "não", 1)){
+            atualizar_etapa($id, 5);
+            $resposta = "Vá conversar com a siri então.";
+        }
+    }
+    if($etapa == 5){
+        atualizar_etapa($id,0);
+        $resposta = "Vejá quem voltou?!?!, Só me procura quando tens problemas, não é mesmo? \n Você sabe qual a categoria do seu problema?";
+    }
 
    /* if($mensagem == verificarCategoria($mensagem)) {
 
@@ -103,7 +162,7 @@ function verificar_usuario_bd($id){
     $db = abrir_banco();
       $consulta = $db->query("SELECT * FROM usuario where id_usuario = $id")->fetchAll(); //fetchall retorna o resultado da query
        if (count($consulta) == 0) { //comparar o resultado para verificar se ja esta no banco
-           $db->query("INSERT INTO usuario (id_usuario,nome, etapa) VALUES($id, 'oi', 0)"); // senão sestiver no banco insere com nome padrão
+           $db->query("INSERT INTO usuario (id_usuario,nome, etapa) VALUES($id, 'oi', 1)"); // senão sestiver no banco insere com nome padrão
            return 1;
        }
        $consulta = $db->query("SELECT * FROM usuario where id_usuario = $id and nome = 'oi'")->fetchAll();
@@ -117,12 +176,11 @@ function atualizar_nome($message,$sender){
     $db->query("UPDATE usuario set nome='$message' where id_usuario =$sender");// atualizando o nome com o texto da mensagem do usuário
 }
 
-/*function atualizar_etapa($id){ pode ser útil, mas não assim
+function atualizar_etapa($id, $etapa){
     $db = abrir_banco();
-    $etapa = $db ->query("SELECT * from usuario")
-    $consulta = $db ->query("UPDATE usuario SET etapa = $etapa where id_usuario = $id") ->fetchAll();
+    $db ->query("UPDATE usuario SET etapa = $etapa where id_usuario = $id");
 
-}*/
+}
 
 function verificarObjeto($message){
     $consulta = $db->query("SELECT * FROM "); //teste de bosta
